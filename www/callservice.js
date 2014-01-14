7b72154cc6f9023d4d2a47debcc9cd69a34bfcfb
morl99 <choffmann87@gmail.com>
2014-01-14 15:07:54 +0100

    T+E

--- www/callservice.js
+++ www/callservice.js
@@ -1,31 +1,23 @@
-(function( cordova ) {
+(function(cordova) {
 
-	function CallService() {}
+	function CallService() {
+	}
 
 	CallService.prototype.start = function(description) {
-		return cordova.exec(
-				function (args) {  } ,
-				function (args) {  } ,
-				"CallService", "start", [description]);
+		return cordova.exec(function(args) {
+		}, function(args) {
+		}, "CallService", "start", [ description ]);
 	};
 
 	CallService.prototype.stop = function() {
-		return cordova.exec(
-				function (args) {  } ,
-				function (args) {  } ,
-				"CallService", "stop", []);
+		return cordova.exec(function(args) {
+		}, function(args) {
+		}, "CallService", "stop", []);
 	};
 
-
-	if(!window.plugins) {
-		window.plugins = {};
-	}
-
-	if (!window.plugins.CallService) {
-		window.plugins.CallService = new CallService();
-	}
+		window.CallService = new CallService();
 
 	var callService = new CallService();
 	module.exports = callService;
 
-})( window.cordova );
+})(window.cordova);
