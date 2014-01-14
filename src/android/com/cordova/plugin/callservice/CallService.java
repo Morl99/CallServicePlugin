09b4e616b1fd149f25c98853450daa8ee3792196
morl99 <choffmann87@gmail.com>
2014-01-14 15:48:04 +0100

    Fixed NPE

--- src/android/com/cordova/plugin/callservice/CallService.java
+++ src/android/com/cordova/plugin/callservice/CallService.java
@@ -5,28 +5,25 @@ import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
 
 public class CallService extends CordovaPlugin {
-  Activity activity;
   private static final String POWER_TUTOR_SERVICE = "edu.umich.PowerTutor.service.UMLoggerService";
   private Intent serviceIntent;
 
   public CallService() {
-    activity = cordova.getActivity();
     serviceIntent = new Intent();
     serviceIntent.setAction(POWER_TUTOR_SERVICE);
   }
 
   public void startPowerService(String testDescription) {
     serviceIntent.putExtra("testDescription", "PhoneGap_" + testDescription);
-    activity.startService(serviceIntent);
+    cordova.getActivity().startService(serviceIntent);
   }
 
   public void stopPowerService() {
-    activity.stopService(serviceIntent);
+    cordova.getActivity().stopService(serviceIntent);
   }
 
   @Override
