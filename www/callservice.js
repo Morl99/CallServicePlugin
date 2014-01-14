08042f60229b663a81c5f0e4eef9e3ca92a1e9e3
morl99 <choffmann87@gmail.com>
2014-01-14 15:13:26 +0100

    Updated Plugin accoding to documentation

--- www/callservice.js
+++ www/callservice.js
@@ -1,23 +1,19 @@
-(function(cordova) {
+function CallService() {
+}
 
-	function CallService() {
-	}
+CallService.prototype.start = function(description) {
+	return cordova.exec(function(args) {
+	}, function(args) {
+	}, "CallService", "start", [ description ]);
+};
 
-	CallService.prototype.start = function(description) {
-		return cordova.exec(function(args) {
-		}, function(args) {
-		}, "CallService", "start", [ description ]);
-	};
+CallService.prototype.stop = function() {
+	return cordova.exec(function(args) {
+	}, function(args) {
+	}, "CallService", "stop", []);
+};
 
-	CallService.prototype.stop = function() {
-		return cordova.exec(function(args) {
-		}, function(args) {
-		}, "CallService", "stop", []);
-	};
+window.CallService = new CallService();
 
-		window.CallService = new CallService();
-
-	var callService = new CallService();
-	module.exports = callService;
-
-})(window.cordova);
+var callService = new CallService();
+module.exports = callService;
